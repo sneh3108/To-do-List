390b43c078eb8265478751868eb60c987261e5e1
SNEH RANJAN <100509471+sneh3108@users.noreply.github.com>
2024-12-10 12:47:16 +0530

    Update list.js

--- routes/list.js
+++ routes/list.js
@@ -1,124 +1,66 @@
-// const router = require("express").Router();
-// const User = require("../models/user");
-// const List = require("../models/list");
-
-// //create
-// router.post("/addTask", async (req, res) => {
-//   try {
-//     const { title, body, id } = req.body;
-//     const existingUser = await User.findById(id);
-//     if (existingUser) {
-//       const list = new List({ title, body, user: existingUser });
-//       await list.save().then(() => res.status(200).json({ list }));
-//       existingUser.list.push(list);
-//       existingUser.save();
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// //update
-// router.put("/updateTask/:id", async (req, res) => {
-//   try {
-//     const { title, body } = req.body;
-//     const list = await List.findByIdAndUpdate(req.params.id, { title, body });
-//     list.save().then(() => res.status(200).json({ message: "Task Updated" }));
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// //delete
-// router.delete("/deleteTask/:id", async (req, res) => {
-//   try {
-//     const { id } = req.body;
-//     const existingUser = await User.findByIdAndDelete(id,
-//       {$pull:{list:req.params.id}}
-//     );
-//     if (existingUser) {
-//       await List.findByIdAndDelete(req.params.id).then(() =>
-//         res.status(200).json({ message: "Task Deleted" })
-//       );
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// //getTask
-// router.get("/getTasks/:id", async (req, res) => {
-//   try {
-//     const list = await List.find({ user: req.params.id }).sort({
-//       createdAt: -1,
-//     });
-//     if (list.length !== 0) {
-//       res.status(200).json({ list: list });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// module.exports = router;
-
-
-
 const router = require("express").Router();
 const User = require("../models/user");
 const List = require("../models/list");
 
-// Add Task Route
+//create
 router.post("/addTask", async (req, res) => {
   try {
     const { title, body, id } = req.body;
     const existingUser = await User.findById(id);
     if (existingUser) {
       const list = new List({ title, body, user: existingUser });
-      await list.save();
+      await list.save().then(() => res.status(200).json({ list }));
       existingUser.list.push(list);
-      await existingUser.save();
-      res.status(200).json({ list });
+      existingUser.save();
     }
   } catch (error) {
-    res.status(500).json({ message: "Error adding task" });
+    console.log(error);
   }
 });
 
-// Update Task Route
+//update
 router.put("/updateTask/:id", async (req, res) => {
   try {
     const { title, body } = req.body;
     const list = await List.findByIdAndUpdate(req.params.id, { title, body });
-    await list.save();
-    res.status(200).json({ message: "Task Updated" });
+    list.save().then(() => res.status(200).json({ message: "Task Updated" }));
   } catch (error) {
-    res.status(500).json({ message: "Error updating task" });
+    console.log(error);
   }
 });
 
-// Delete Task Route
+//delete
 router.delete("/deleteTask/:id", async (req, res) => {
   try {
-    await List.findByIdAndDelete(req.params.id);
-    await User.updateOne(
-      { list: req.params.id },
-      { $pull: { list: req.params.id } }
-    );
-    res.status(200).json({ message: "Task Deleted" });
+    const { id } = req.body;
+    const existingUser = await User.findByIdAndDelete(id,
+      {$pull:{list:req.params.id}}
+    );
+    if (existingUser) {
+      await List.findByIdAndDelete(req.params.id).then(() =>
+        res.status(200).json({ message: "Task Deleted" })
+      );
+    }
   } catch (error) {
-    res.status(500).json({ message: "Error deleting task" });
+    console.log(error);
   }
 });
 
-// Get Tasks Route
+//getTask
 router.get("/getTasks/:id", async (req, res) => {
   try {
-    const list = await List.find({ user: req.params.id }).sort({ createdAt: -1 });
-    res.status(200).json({ list });
+    const list = await List.find({ user: req.params.id }).sort({
+      createdAt: -1,
+    });
+    if (list.length !== 0) {
+      res.status(200).json({ list: list });
+    }
   } catch (error) {
-    res.status(500).json({ message: "Error fetching tasks" });
+    console.log(error);
   }
 });
 
 module.exports = router;
+
+
+
