52c5cc770d783611e9ab4d985de99ab71b3955b3
SNEH RANJAN <100509471+sneh3108@users.noreply.github.com>
2024-12-10 12:49:25 +0530

    Update conn.js

--- conn/conn.js
+++ conn/conn.js
@@ -1,22 +1,5 @@
 
 
-// const mongoose = require("mongoose");
-
-// const conn = async (req, res) => {
-//   try {
-//     await mongoose
-//       .connect(
-//         "mongodb+srv://snehr63:4OKTF6WzxxbfMQgi@cluster0.w1ulb.mongodb.net/"
-//       )
-//       .then(() => {
-//         console.log("Connected");
-//       });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-// conn();
-
 const mongoose = require("mongoose");
 
 const conn = async () => {
