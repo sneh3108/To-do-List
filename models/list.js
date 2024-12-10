5309c58062b1eaac50e04d1cc6500a8ad21c0ea0
SNEH RANJAN <100509471+sneh3108@users.noreply.github.com>
2024-12-10 12:48:14 +0530

    Update list.js

--- models/list.js
+++ models/list.js
@@ -1,31 +1,4 @@
-// const mongoose = require("mongoose");
-// const listSchema = new mongoose.Schema(
-//   {
-//     title: {
-//       type: String,
-//       required: true,
-//     },
-//     body: {
-//       type: String,
-//       required: true,
-//     },
-//     user: [
-//       {
-//         type: mongoose.Types.ObjectId,
-//         ref: "User",
-//       },
-//     ],
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model("List", listSchema);
-
-
-
-
 const mongoose = require("mongoose");
-
 const listSchema = new mongoose.Schema(
   {
     title: {
@@ -47,3 +20,4 @@ const listSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("List", listSchema);
+
