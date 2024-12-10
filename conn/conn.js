5683ffef71869877eb9c95d1fc33e2409d215f50
SNEH RANJAN <100509471+sneh3108@users.noreply.github.com>
2024-12-10 12:31:16 +0530

    Update conn.js

--- conn/conn.js
+++ conn/conn.js
@@ -1,18 +1,34 @@
 
 
+// const mongoose = require("mongoose");
+
+// const conn = async (req, res) => {
+//   try {
+//     await mongoose
+//       .connect(
+//         "mongodb+srv://snehr63:4OKTF6WzxxbfMQgi@cluster0.w1ulb.mongodb.net/"
+//       )
+//       .then(() => {
+//         console.log("Connected");
+//       });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// conn();
+
 const mongoose = require("mongoose");
 
-const conn = async (req, res) => {
+const conn = async () => {
   try {
-    await mongoose
-      .connect(
-        "mongodb+srv://snehr63:4OKTF6WzxxbfMQgi@cluster0.w1ulb.mongodb.net/"
-      )
-      .then(() => {
-        console.log("Connected");
-      });
+    await mongoose.connect(
+      "mongodb+srv://snehr63:4OKTF6WzxxbfMQgi@cluster0.w1ulb.mongodb.net/",
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
   }
 };
+
 conn();
