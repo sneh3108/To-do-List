a1953e71f62966d51ae863461dd03123d4b6e4cc
SNEH RANJAN <100509471+sneh3108@users.noreply.github.com>
2024-12-10 12:03:48 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,96 +1,41 @@
-// const router = require("express").Router();
-// const User = require("../models/user");
-// const bcrypt = require("bcryptjs");
-// //SIGN UP
-// router.post("/register", async (req, res) => {
-//   try {
-//     const { email, username, password } = req.body;
-//     const hashpassword = bcrypt.hashSync(password);
-//     const user = new User({ email, username, password: hashpassword });
-//     await user
-//       .save()
-//       .then(() => res.status(200).json({ message: "Sign Up Successfull" }));
-//   } catch (error) {
-//     res.status(200).json({ message: "User Already Exists" });
-//   }
-// });
-
-// //SIGN IN
-
-// router.post("/signin", async (req, res) => {
-//   try {
-//     const user = await User.findOne({ email: req.body.email });
-//     if (!user) {
-//       return res
-//         .status(400)
-//         .json({ message: "User not found. Please Sign Up First" });
-//     }
-
-//     const isPasswordCorrect = bcrypt.compareSync(
-//       req.body.password,
-//       user.password
-//     );
-//     if (!isPasswordCorrect) {
-//       return res.status(400).json({ message: "Password is not correct" });
-//     }
-
-//     const { password, ...others } = user._doc;
-//     res.status(200).json({ user: others });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// });
-
-// module.exports = router;
-
 const router = require("express").Router();
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
-
 //SIGN UP
 router.post("/register", async (req, res) => {
   try {
     const { email, username, password } = req.body;
-
-    // Check if the user already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "User Already Exists" });
-    }
-
-    // Hash the password and save the user
-    const hashpassword = bcrypt.hashSync(password, 10);
+    const hashpassword = bcrypt.hashSync(password);
     const user = new User({ email, username, password: hashpassword });
-    await user.save();
-
-    // Respond with success message and user id
-    res.status(200).json({ message: "Sign Up Successful", id: user._id });
+    await user
+      .save()
+      .then(() => res.status(200).json({ message: "Sign Up Successfull" }));
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(200).json({ message: "User Already Exists" });
   }
 });
 
 //SIGN IN
+
 router.post("/signin", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-
-    // If user does not exist
     if (!user) {
-      return res.status(400).json({ message: "User not found. Please Sign Up First" });
+      return res
+        .status(400)
+        .json({ message: "User not found. Please Sign Up First" });
     }
 
-    // Check if the password matches
-    const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.password);
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
     if (!isPasswordCorrect) {
       return res.status(400).json({ message: "Password is not correct" });
     }
 
-    // Send user id and other details
     const { password, ...others } = user._doc;
-    res.status(200).json({ message: "Sign In Successful", id: user._id, user: others });
+    res.status(200).json({ user: others });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
@@ -98,3 +43,5 @@ router.post("/signin", async (req, res) => {
 });
 
 module.exports = router;
+
+
