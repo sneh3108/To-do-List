5c4f6ec73460c2ee7168a5824b7a77e4e20cd7f7
SNEH RANJAN <100509471+sneh3108@users.noreply.github.com>
2024-12-10 12:40:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -49,44 +49,4 @@
 // export default App;
 
 
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { authActions } from "./store";
-import Navbar from "./components/navbar/Navbar";
-import Home from "./components/home/Home";
-import Footer from "./components/footer/Footer";
-import About from "./components/about/About";
-import Signup from "./components/signup/Signup";
-import SignIn from "./components/signup/SignIn";
-import Todo from "./components/todo/Todo";
-
-const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const id = sessionStorage.getItem("id");
-    if (id) {
-      dispatch(authActions.login());
-    }
-  }, [dispatch]);
-
-  return (
-    <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/todo" element={<Todo />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<SignIn />} />
-        </Routes>
-      </Router>
-      <Footer />
-    </div>
-  );
-};
-
-export default App;
 
