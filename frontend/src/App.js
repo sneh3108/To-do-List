60baaad118b88cd4612150129e57e266132445e6
SNEH RANJAN <100509471+sneh3108@users.noreply.github.com>
2024-12-10 12:41:06 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,48 @@
-// import React, { useEffect } from "react";
-// import Navbar from "./components/navbar/Navbar";
-// import Home from "./components/home/Home";
+import React, { useEffect } from "react";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
 
-// import Footer from "./components/footer/Footer";
-// import About from "./components/about/About";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Signup from "./components/signup/Signup";
-// import SignIn from "./components/signup/SignIn";
-// import Todo from "./components/todo/Todo";
-// import { useDispatch } from "react-redux";
-// import { authActions } from "./store";
-
-
+import Footer from "./components/footer/Footer";
+import About from "./components/about/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./components/signup/Signup";
+import SignIn from "./components/signup/SignIn";
+import Todo from "./components/todo/Todo";
+import { useDispatch } from "react-redux";
+import { authActions } from "./store";
 
 
 
-
-
-// const App = () => {
-//   const dispatch = useDispatch();
-//   useEffect(() => {
-//     const id = sessionStorage.getItem("id");
-//     if (id) {
-//       dispatch(authActions.login());
-//     }
-//   }, []);
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const id = sessionStorage.getItem("id");
+    if (id) {
+      dispatch(authActions.login());
+    }
+  }, []);
 
 
 
-//   return (
-//     <div>
-//       <Router>
-//         <Navbar />
-//         <Routes>
-//           <Route exact path="/" element={<Home />} />
-//           <Route path="/about" element={<About />} />
-//           <Route path="/todo" element={<Todo />} />
-//           <Route path="/signup" element={<Signup />} />
-//           <Route path="/signin" element={<SignIn/>} />
-//         </Routes>
-//       </Router>
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/todo" element={<Todo />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<SignIn/>} />
+        </Routes>
+      </Router>
 
-//       <Footer />
-//     </div>
-//   );
-// };
+      <Footer />
+    </div>
+  );
+};
 
-// export default App;
+export default App;
 
 
 
