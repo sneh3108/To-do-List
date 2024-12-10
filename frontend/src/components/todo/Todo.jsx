2fe72026800b71f4244969beb8ef9046c9960ed3
SNEH RANJAN <100509471+sneh3108@users.noreply.github.com>
2024-12-10 11:46:43 +0530

    Update Todo.jsx

--- frontend/src/components/todo/Todo.jsx
+++ frontend/src/components/todo/Todo.jsx
@@ -1,151 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import "./todo.css";
-// import TodoCards from "./TodoCards";
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import Update from "./Update";
-// import axios from "axios";
-// let id = sessionStorage.getItem("id");
-// let toUpdateArray = [];
-// const Todo = () => {
-//   const [Inputs, setInputs] = useState({
-//     title: "",
-//     body: "",
-//   });
-//   const [Array, setArray] = useState([]);
-
-//   const show = () => {
-//     document.getElementById("textarea").style.display = "block";
-//   };
-//   const change = (e) => {
-//     const { name, value } = e.target;
-//     setInputs({ ...Inputs, [name]: value });
-//   };
-//   const submit = async () => {
-//     if (Inputs.title === "" || Inputs.body === "") {
-//       toast.error("Title Or Body Can't Be Empty");
-//     } else {
-//       if (id) {
-//         await axios
-//           .post(`${window.location.origin}/api/v2/addTask`, {
-//             title: Inputs.title,
-//             body: Inputs.body,
-//             id: id,
-//           })
-//           .then((response) => {
-//             console.log(response);
-//           });
-//         setInputs({ title: "", body: "" });
-//         toast.success("Your Task Is Added");
-//       } else {
-//         setArray([...Array, Inputs]);
-//         setInputs({ title: "", body: "" });
-//         toast.success("Your Task Is Added");
-//         toast.error("Your Task Is Not Saved ! Please SignUp");
-//       }
-//     }
-//   };
-
-//   const del = async (Cardid) => {
-//     if (id) {
-//       await axios
-//         .delete(`${window.location.origin}/api/v2/deleteTask/${Cardid}`, {
-//           data: { id: id },
-//         })
-//         .then(() => {
-//           toast.success("Your Task Is Deleted");
-//         });
-//     } else {
-//       toast.error("Please SignUp First");
-//     }
-//   };
-
-//   const dis = (value) => {
-//     document.getElementById("todo-update").style.display = value;
-//   };
-//   const update = (value) => {
-//     toUpdateArray = Array[value];
-//   };
-//   useEffect(() => {
-//     if (id) {
-//       const fetch = async () => {
-//         await axios
-//           .get(`${window.location.origin}/api/v2/getTasks/${id}`)
-//           .then((response) => {
-//             setArray(response.data.list);
-//           });
-//       };
-//       fetch();
-//     }
-//   }, [submit]);
-
-//   return (
-//     <>
-//       <div className="todo">
-//         <ToastContainer />
-//         <div className="todo-main container d-flex justify-content-center align-items-center my-4 flex-column">
-//           <div className="d-flex flex-column todo-inputs-div w-lg-50 w-100 p-1">
-//             <input
-//               type="text"
-//               placeholder="Task Headline"
-//               className="my-2 p-2 todo-inputs"
-//               onClick={show}
-//               name="title"
-//               value={Inputs.title}
-//               onChange={change}
-//             />
-//             <textarea
-//               id="textarea"
-//               type="text"
-//               placeholder="Here goes your Description"
-//               name="body"
-//               className=" p-2 todo-inputs"
-//               value={Inputs.body}
-//               onChange={change}
-//             />
-//           </div>
-//           <div className=" w-50 w-100 d-flex justify-content-end my-3">
-//             <button className="home-btn px-2 py-1" onClick={submit}>
-//               Go
-//             </button>
-//           </div>
-//         </div>
-//         <div className="todo-body">
-//           <div className="container-fluid">
-//             <div className="row ">
-//               {Array &&
-//                 Array.map((item, index) => (
-//                   <div
-//                     className="col-lg-3 col-11 mx-lg-5 mx-3 my-2"
-//                     key={index}
-//                   >
-//                     <TodoCards
-//                       title={item.title}
-//                       body={item.body}
-//                       id={item._id}
-//                       delid={del}
-//                       display={dis}
-//                       updateId={index}
-//                       toBeUpdate={update}
-//                     />
-//                   </div>
-//                 ))}
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="todo-update " id="todo-update">
-//         <div className="container update">
-//           <Update display={dis} update={toUpdateArray} />
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Todo;
-
-
 import React, { useEffect, useState } from "react";
 import "./todo.css";
 import TodoCards from "./TodoCards";
@@ -153,88 +5,79 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Update from "./Update";
 import axios from "axios";
-
+let id = sessionStorage.getItem("id");
+let toUpdateArray = [];
 const Todo = () => {
-  const [Inputs, setInputs] = useState({ title: "", body: "" });
+  const [Inputs, setInputs] = useState({
+    title: "",
+    body: "",
+  });
   const [Array, setArray] = useState([]);
-  const [id, setId] = useState(sessionStorage.getItem("id") || null);
-  const [toUpdateArray, setToUpdateArray] = useState(null);
-  const [showTextarea, setShowTextarea] = useState(false);
 
-  // Sync sessionStorage with React state
-  useEffect(() => {
-    const handleStorageChange = () => {
-      setId(sessionStorage.getItem("id")); // Update state whenever sessionStorage changes
-    };
-
-    window.addEventListener("storage", handleStorageChange);
-    return () => window.removeEventListener("storage", handleStorageChange);
-  }, []);
-
-  const show = () => setShowTextarea(true);
-
+  const show = () => {
+    document.getElementById("textarea").style.display = "block";
+  };
   const change = (e) => {
     const { name, value } = e.target;
     setInputs({ ...Inputs, [name]: value });
   };
-
   const submit = async () => {
     if (Inputs.title === "" || Inputs.body === "") {
       toast.error("Title Or Body Can't Be Empty");
-    } else if (id) {
-      try {
-        await axios.post(`${window.location.origin}/api/v2/addTask`, {
-          title: Inputs.title,
-          body: Inputs.body,
-          id: id,
-        });
+    } else {
+      if (id) {
+        await axios
+          .post(`${window.location.origin}/api/v2/addTask`, {
+            title: Inputs.title,
+            body: Inputs.body,
+            id: id,
+          })
+          .then((response) => {
+            console.log(response);
+          });
         setInputs({ title: "", body: "" });
         toast.success("Your Task Is Added");
-      } catch (error) {
-        toast.error("Failed to add task");
-        console.error(error);
+      } else {
+        setArray([...Array, Inputs]);
+        setInputs({ title: "", body: "" });
+        toast.success("Your Task Is Added");
+        toast.error("Your Task Is Not Saved ! Please SignUp");
       }
-    } else {
-      setArray([...Array, Inputs]);
-      setInputs({ title: "", body: "" });
-      toast.success("Your Task Is Added");
-      toast.error("Your Task Is Not Saved! Please SignUp");
     }
   };
 
   const del = async (Cardid) => {
     if (id) {
-      try {
-        await axios.delete(
-          `${window.location.origin}/api/v2/deleteTask/${Cardid}`,
-          { data: { id: id } }
-        );
-        toast.success("Your Task Is Deleted");
-      } catch (error) {
-        toast.error("Failed to delete task");
-        console.error(error);
-      }
+      await axios
+        .delete(`${window.location.origin}/api/v2/deleteTask/${Cardid}`, {
+          data: { id: id },
+        })
+        .then(() => {
+          toast.success("Your Task Is Deleted");
+        });
     } else {
       toast.error("Please SignUp First");
     }
   };
 
+  const dis = (value) => {
+    document.getElementById("todo-update").style.display = value;
+  };
+  const update = (value) => {
+    toUpdateArray = Array[value];
+  };
   useEffect(() => {
     if (id) {
       const fetch = async () => {
-        try {
-          const response = await axios.get(
-            `${window.location.origin}/api/v2/getTasks/${id}`
-          );
-          setArray(response.data.list);
-        } catch (error) {
-          toast.error("Failed to fetch tasks");
-          console.error(error);
-        }
+        await axios
+          .get(`${window.location.origin}/api/v2/getTasks/${id}`)
+          .then((response) => {
+            setArray(response.data.list);
+          });
       };
       fetch();
     }
-  }, [id]); // Fetch tasks whenever `id` changes
+  }, [submit]);
 
   return (
     <>
@@ -253,16 +96,15 @@ const Todo = () => {
             />
             <textarea
               id="textarea"
-              style={{ display: showTextarea ? "block" : "none" }}
               type="text"
               placeholder="Here goes your Description"
               name="body"
-              className="p-2 todo-inputs"
+              className=" p-2 todo-inputs"
               value={Inputs.body}
               onChange={change}
             />
           </div>
-          <div className="w-50 w-100 d-flex justify-content-end my-3">
+          <div className=" w-50 w-100 d-flex justify-content-end my-3">
             <button className="home-btn px-2 py-1" onClick={submit}>
               Go
             </button>
@@ -270,7 +112,7 @@ const Todo = () => {
         </div>
         <div className="todo-body">
           <div className="container-fluid">
-            <div className="row">
+            <div className="row ">
               {Array &&
                 Array.map((item, index) => (
                   <div
@@ -282,9 +124,9 @@ const Todo = () => {
                       body={item.body}
                       id={item._id}
                       delid={del}
-                      display={setShowTextarea}
+                      display={dis}
                       updateId={index}
-                      toBeUpdate={setToUpdateArray}
+                      toBeUpdate={update}
                     />
                   </div>
                 ))}
@@ -292,9 +134,9 @@ const Todo = () => {
           </div>
         </div>
       </div>
-      <div className="todo-update" id="todo-update">
+      <div className="todo-update " id="todo-update">
         <div className="container update">
-          <Update display={setShowTextarea} update={toUpdateArray} />
+          <Update display={dis} update={toUpdateArray} />
         </div>
       </div>
     </>
@@ -302,6 +144,8 @@ const Todo = () => {
 };
 
 export default Todo;
+
+
 
 
 
